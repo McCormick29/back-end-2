9ccaa53c7339af7721617f4074a8f229ba486628
McCormick29 <68787133+McCormick29@users.noreply.github.com>
2021-07-06 14:21:39 -0600

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const ctrl = require('./controller')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/api/houses', ctrl.getHouses)
+app.delete('/api/houses/:id', ctrl.deleteHouse)
+app.post('/api/houses', ctrl.createHouse)
+app.put('/api/houses/:id', ctrl.updateHouse)
+
+
+
+
+
+
+app.listen(4004, () => console.log('running on 4004'));
